02b85aada2926badef0e5d78347fc17d72f097f9
BC <ben@benclennon.com>
2025-06-09 19:01:54 -0500

    Initial setup: Add package.json, ESLint config, and .gitignore - Set up npm scripts, ESLint with HTML plugin, serve for dev server, and comprehensive .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'html'
+  ],
+  rules: {
+    // Add any custom rules here
+  }
+}; 
